7f59c8eded8de5e3e1ab4b2b0f08a053dc617138
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2024-05-15 22:28:10 -0600

    Dolly 2

--- MensoRacingProyect/Assets/Die.cs
+++ MensoRacingProyect/Assets/Die.cs
@@ -9,23 +9,16 @@ public class Die : MonoBehaviour
     [SerializeField] private CheckChecker checker;
    [SerializeField] private CharacterController cc;
    [SerializeField] private GameObject player;
+   [SerializeField] private GameObject playerToMove;
    [SerializeField] private float moveSpeed = 500;
 
    public void OnTriggerEnter(Collider other)
    {
        if (other.CompareTag("Player"))
        {
-           MoveTowardsTarget(checker.currentPointLoad.position);
+           
        }
    }
 
-   void MoveTowardsTarget(Vector3 target)
-   {
-       Debug.Log("entro");
-       cc = player.GetComponent<CharacterController>();
-       var offset = target - transform.position;
-       offset = offset.normalized * moveSpeed;
-       cc.Move(offset * Time.deltaTime);
-
-   }
+  
 }
